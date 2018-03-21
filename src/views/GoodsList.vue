98c3068381967c4a5e1d81eab47575a11218160d
lvsun <573741688@qq.com>
2018-03-21 17:38:41 +0800

    初始化

---
+++ src/views/GoodsList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    这是商品列表
+    <span>{{$route.params.goodsId}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg:'hello vue'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
